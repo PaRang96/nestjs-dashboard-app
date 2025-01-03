dbe5ecb1aae94f607b59c6796f612f3d1560d743
Lee, Min Seok <winterreise94@gmail.com>
2025-01-03 23:24:23 +0900

    initial commit

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+    constructor(private boardsService: BoardsService) {}
+
+
+}
